f53d342a9ebf24813a948c6e18100a85b1a147f3
LauraJazmin <laura.jazmin28@gmail.com>
2020-03-21 11:19:47 -0500

    Updates

---
+++ precache-manifest.ae6a7c86895443b4a08c271cf3a20cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5588baef2ed305e5dd10eec0f56edcc8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "386005f77961e8ee50da",
+    "url": "/monsters-rolodex/static/css/main.37f247ed.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386005f77961e8ee50da",
+    "url": "/monsters-rolodex/static/js/main.a748fec3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
